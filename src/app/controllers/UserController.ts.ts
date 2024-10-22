689090ea85292c5f10da5ab472ecd49ceefacb70
Gustavo <gustavo303a@gmail.com>
2024-10-22 20:17:33 -0300

    update

--- src/app/controllers/UserController.ts.ts
+++ src/app/controllers/UserController.ts.ts
@@ -1,5 +1,8 @@
 import { Request, Response, Router } from "express";
 import { AppDataSource } from "../../database/data-source";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const SECRET = "tools";
 
 const db = AppDataSource;
 
@@ -8,7 +11,7 @@ const userRouter = Router();
 //Ver usuarios
 userRouter.get("/profile", async (_req: Request, res: Response) => {
   try {
-    const result = await db.query("SELECT * FROM users;");
+    const result = await db.query("SELECT * FROM usuarios;");
     return res.status(200).json({ result });
   } catch (erro) {
     return res
@@ -21,19 +24,71 @@ userRouter.get("/profile", async (_req: Request, res: Response) => {
 userRouter.post("/register", async (req: Request, res: Response) => {
   const { email, senha } = req.body;
 
+  //Hasheando a senha
+  const hashedPassword = await bcrypt.hash(senha, 10);
+
   if (!email || !senha) {
     return res.status(400).json({ mensagem: "Email e senha são obrigatórios" });
   }
 
   try {
-    const query = "INSERT INTO users (email, senha) VALUES (?, ?)";
-    const result = await db.query(query, [email, senha]);
+    const query = "INSERT INTO usuarios (email, senha) VALUES ($1, $2)";
+    const result = await db.query(query, [email, hashedPassword]);
 
     return res
       .status(201)
-      .json({ mensagem: `Usuário criado com sucesso! ID: ${result}` });
+      .json({ mensagem: `Usuário criado com sucesso! ID: ${result.insertId}` });
   } catch (erro) {
+    console.error(erro);
     return res.status(500).json({ mensagem: "Erro ao cadastrar usuário" });
+  }
+});
+
+//Verificão JWT
+function verifyJWT(req: Request, res: Response) {
+  const token = req.headers["authorization"]?.split("")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "Token não fornecido" });
+  }
+
+  jwt.verify(token, SECRET, (error, result) => {
+    if (error) {
+      return res.status(401).json({ error: "Token inválido ou expirado" });
+    }
+  });
+}
+
+//Logar usuario
+userRouter.post("/login", async (req: Request, res: Response) => {
+  const { email, senha } = req.body;
+
+  if (!email || !senha) {
+    return res.status(400).json({ erro: "Email e senha são obrigatórios" });
+  }
+
+  try {
+    const result = await db.query("SELECT * FROM usuarios WHERE email = $1", [
+      email,
+    ]);
+
+    if (!result || !result.rows || result.rows.length === 0) {
+      return res.status(404).json({ erro: "Usuário não encontrado" });
+    }
+
+    const usuario = result.rows[0];
+    const match = await bcrypt.compare(senha, usuario.senha);
+
+    if (!match) {
+      return res.status(401).json({ erro: "Senha incorreta" });
+    }
+
+    const token = jwt.sign({ userId: usuario.id }, SECRET, { expiresIn: "5m" });
+    return res
+      .status(200)
+      .json({ auth: true, token, mensagem: "Login bem-sucedido" });
+  } catch (error) {
+    return res.status(500).json({ erro: "Erro ao buscar usuário" });
   }
 });
 
--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -5,5 +5,6 @@ const routers = Router();
 
 routers.get("/profile", userRouter);
 routers.post("/register", userRouter);
+routers.post("/login", userRouter);
 
 export default routers;
